c40707fc0b23867d48ffcee415c519791d459279
Sana Jahan <sanadgrt@gmail.com>
2016-09-20 00:27:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
+var articles = {
+    article-one : {
     title : 'Sana Jahan|ArticleOne',
     heading : 'Article One',
     date : 'Sep 5 , 2016',
@@ -17,7 +18,24 @@ var articleOne = {
              <p>
                 This is the content of my first article .This is the content of my first article .This is the content of my first article .This is the content of my first article .This is the content of my first article .
             </p> `
-};
+},
+ article-two :{
+     title : 'Sana Jahan|ArticleTwo',
+    heading : 'Article One',
+    date : 'Sep 10 , 2016',
+    content : `
+    <p>
+        This is second article !!!!!!
+            </p> `},
+ article-three : {title : 'Sana Jahan|ArticleThree',
+    heading : 'Article Three',
+    date : 'Sep 15 , 2016',
+    content : `
+    <p>
+              Johnny Johnny yes papa, Eating sugar no papa !!!
+            </p>
+             `}
+  };
 
 function createTemplate(data){
     var title = data.title;
@@ -54,8 +72,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
