4c8173122dee96fe488ca214066a85d816e258b6
Sana Jahan <sanadgrt@gmail.com>
2016-09-30 22:10:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+var counter = 0;
+button.onclick= function(){
+    //Make request to counter endpoint
+    
+    
+    // Capture the response and store in a variabvle
+    
+    
+    
+    //Render the variable in correct span
+    
+    counter =  counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML=counter.toString();
+}
