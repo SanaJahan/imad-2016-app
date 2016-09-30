81ca3d1c9e8c70c85a3f282ce9db209902601bc4
Sana Jahan <sanadgrt@gmail.com>
2016-09-30 22:57:24 +0530

    made counter locally

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,27 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+button.onclick = function() {
+    //Create a request
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if(request.readystate===XMLHttpRequest.DONE){
+          if(request.status===200){
+              var counter=request.responseText;
+              var span = document.getElementById('count');
+             span.innerHTML= counter.toString();
+          }
+      }
+        
+    };
+    
+    
+    // Capture the response and store in a variabvle
+    
+    
+    
+    //Make request
+    request.open('GET','http://sanajahan.imad.hasura-app.io/counter',true);
+    request.send(null);
+  
+    
+};
