41c6b8ad39cb1c2c1883be80752fbb9745033298
Sana Jahan <sanadgrt@gmail.com>
2016-10-13 02:33:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ button.onclick = function() {
 };
 // Submit name
 
-var submit = document.getElementById('submit-btn');
+var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request to server and send the names
      var request = new XMLHttpRequest();
