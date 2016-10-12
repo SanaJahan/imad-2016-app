cfe4e11c225c1af8766f0c252d157bd2fb93c728
Sana Jahan <sanadgrt@gmail.com>
2016-10-13 02:28:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,3 +27,32 @@ button.onclick = function() {
   
     
 };
+// Submit name
+
+var submit = document.getElementById('submit-btn');
+submit.onclick = function(){
+    //make a request to server and send the names
+     var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if(request.readyState===XMLHttpRequest.DONE){
+          if(request.status===200){
+    
+    //Capture the names and render it 
+    var names = request.responseText;
+    names = JSON.parse(names);
+    for(var i = 0; i<names.length;i++){
+        list += '<li>'+names[i]+'</li>';
+    }
+    var ul = document.getElementById(namelist);
+    ul.innerHTML = list;
+          }
+      }
+          };
+     //Make request
+     var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://sanajahan.imad.hasura-app.io/submit-name?name='+name,true);
+   //for local machine 
+   //request.open('GET',document.URL+'counter',true);
+    request.send(null);
+};
