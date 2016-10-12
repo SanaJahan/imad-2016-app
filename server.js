7465859037c2eae47efd5025982c968f0bfb777b
Sana Jahan <sanadgrt@gmail.com>
2016-10-13 03:00:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ var articles = {
     heading : 'Article Three',
     date : 'Sep 15 , 2016',
     content : `
-    <head><style> #commentbox3{min height = "40px"; }</style></head>
+    <head><style> #commentbox3{min-height = "40px"; }</style></head>
     <p>
               Johnny Johnny yes papa, Eating sugar no papa !!!
             </p>
