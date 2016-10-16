b42ba7af00935a0e4842146b77dc6b343b1bfb47
Sana Jahan <sanadgrt@gmail.com>
2016-10-17 03:55:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,3 +57,33 @@ submit.onclick = function(){
    //request.open('GET',document.URL+'counter',true);
     request.send(null);
 };
+// Submit a comment
+var commentbtn = document.getElementById('comment-button');
+commentbtn.onclick = function(){
+    //Make request to server to send the comments
+    var request1 = new XMLHttpRequest();
+    request1.onreadystatechange = function (){
+        if(request1.readyState===XMLHttpRequest.DONE){
+            if(request1.status===200){
+    //Store the comments and display on the page
+    var comments = request1.responseText;
+        comments = JSON.parse(comments);
+        list_comment = '';
+        for(var j = 0; j<comments.length;j++){
+            list_comment += '<li>'+comments[j]+'</li>';
+        }
+            var ul1 = document.getElementById('commentlist');
+            ul1.innerHTML = list_comment;
+            }
+        }
+    };
+    
+   
+    //Make request to capture the comment
+    var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+     request.open('GET','http://sanajahan.imad.hasura-app.io/submit-comment?comment='+comment,true);
+   //for local machine 
+   //request.open('GET',document.URL+'counter',true);
+    request.send(null);
+};
