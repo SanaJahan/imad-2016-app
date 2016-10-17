1fff04e171136d0ff9e1283c551766fc94cd726d
Sana Jahan <sanadgrt@gmail.com>
2016-10-18 02:34:37 +0530

    Create article.js

---
+++ ui/article.js
@@ -0,0 +1,16 @@
+<html>
+<head> 
+  <title> Test Article 1 </title> </head>
+  <body>
+    <p> Blavjhgc b rb hkbvgt ebhrijb dfkhihf ekb rwgn vd  vsna x nanjb    rfbgo4v n m cjbgh vnknv nk  .
+    Blavjhgc b rb hkbvgt ebhrijb dfkhihf ekb rwgn vd  vsna x nanjb    rfbgo4v n m cjbgh vnknv nk 
+    Blavjhgc b rb hkbvgt ebhrijb dfkhihf ekb rwgn vd  vsna x nanjb    rfbgo4v n m cjbgh vnknv nk 
+    Blavjhgc b rb hkbvgt ebhrijb dfkhihf ekb rwgn vd  vsna x nanjb    rfbgo4v n m cjbgh vnknv nk 
+    Blavjhgc b rb hkbvgt ebhrijb dfkhihf ekb rwgn vd  vsna x nanjb    rfbgo4v n m cjbgh vnknv nk 
+    </p>
+    <div class = " Comments"> <p> <input type = "text" name = "Comment_1" placeholder = "Add public comment" /> </p> </div>
+    <div class = "submit_comment"> <p> < button> Submit </button> </p> </div> 
+   </body>
+ </html>
+   
+     
