003687673b756b00e660b61937b293a93d1e46fa
Sana Jahan <sanadgrt@gmail.com>
2016-10-18 02:35:27 +0530

    Update article.js

--- ui/article.js
+++ ui/article.js
@@ -8,7 +8,7 @@
     Blavjhgc b rb hkbvgt ebhrijb dfkhihf ekb rwgn vd  vsna x nanjb    rfbgo4v n m cjbgh vnknv nk 
     Blavjhgc b rb hkbvgt ebhrijb dfkhihf ekb rwgn vd  vsna x nanjb    rfbgo4v n m cjbgh vnknv nk 
     </p>
-    <div class = " Comments"> <p> <input type = "text" name = "Comment_1" placeholder = "Add public comment" /> </p> </div>
+    <div class = "Comments"> <p> <input type = "text" name = "Comment_1" placeholder = "Add public comment" /> </p> </div>
     <div class = "submit_comment"> <p> < button> Submit </button> </p> </div> 
    </body>
  </html>
