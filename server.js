d8a9cbcd80130f37a2dd5322766d76ad21cf193c
Sana Jahan <sanadgrt@gmail.com>
2016-11-09 01:37:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/hash/:input', function(req, res) {
 
 app.post('/create-user',function(req,res){
   var username = req.body.username;
-  var uname = req.body.uname;
+  var uname = req.body.name;
   var email = req.body.email;
   var password = req.body.password;
   var salt = crypto.randomBytes(128).toString('hex');
