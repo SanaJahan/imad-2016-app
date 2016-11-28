05a6fb71e0bca4509efb3cf0ede98ae9d7dc6506
Sana Jahan <sanadgrt@gmail.com>
2016-11-28 23:06:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -223,10 +223,10 @@ input[type=email]:focus {
                 <div>
                    <p align = "center"><label> ENTER YOUR NAME: </label> <input type="text" id="newusername" placeholder="Your good name" /><br /><br/>
                     <label> ENTER USERNAME: </label><input type="text" id="newuname" placeholder="username" /></br><br>
-                    <label> ENTER EMAIL: </label><input type="email" id="newemail" placeholder="Your email" /><br /><br/>
+                    <label> ENTER EMAIL:   </label><input type="email" id="newemail" placeholder="Your email" /><br /><br/>
                     <label> ENTER PASSWORD: </label><input type="password" id="newpassword" /></p>
         <br/><br/>
-        <p align = "center"><button id="register_btn" value="Register" /></button> </p>                             
+        <p align = "center"><button id="register_btn" value="Register" />Register</button> </p>                             
             <br>
                     <center><a href = "/loginUser">Already have an account? Sign In </a> </center>
                 </div>
