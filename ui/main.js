4d96c816c9a889208c898724a4356d232e6d59be
Sana Jahan <sanadgrt@gmail.com>
2016-12-17 03:03:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -144,36 +144,8 @@ var register = document.getElementById('register_btn');
    }
 
 
-/* Submit a comment
-var commentbtn = document.getElementById('comment-button');
-if (commentbtn != undefined) {
-commentbtn.onclick = function(){
-    //Make request to server to send the comments
-    var request1 = new XMLHttpRequest();
-    request1.onreadystatechange = function (){
-        if(request1.readyState===XMLHttpRequest.DONE){
-            if(request1.status===200){
-    //Store the comments and display on the page
-    var comments = request1.responseText;
-        comments = JSON.parse(comments);
-        list_comment = '';
-        for(var j = 0; j<comments.length;j++){
-            list_comment += '<li>'+comments[j]+'</li>';
-        }
-            var ul1 = document.getElementById('commentlist');
-            ul1.innerHTML = list_comment;
-            }
-        }
-    };
-    
-   
-    //Make request to capture the comment
-    var commentInput = document.getElementById('comment');
-    var comment = commentInput.value;
-     request1.open('GET','http://sanajahan.imad.hasura-app.io/submit-comment?comment='+comment,true);
-   //for local machine 
 
-   request1.open('GET', window.location.protocol+'//'+window.location.host+'/submit-comment?comment='+comment, true);
+ /*  request1.open('GET', window.location.protocol+'//'+window.location.host+'/submit-comment?comment='+comment, true);
    //for local machine 
    //request.open('GET',document.URL+'counter',true);
     request1.send(null);
@@ -181,10 +153,6 @@ commentbtn.onclick = function(){
 }*/
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('loginArea');
-    /*loginArea.innerHTML = `
-        <h3> Hi <i>${username}</i></h3>
-        <a href="/logout">Logout</a>
-    `;*/
 }
 
 function loadLogin () {
