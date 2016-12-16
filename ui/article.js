a88900bbcceaf493b2b80998e6b4a21daa3e003f
Sana Jahan <sanadgrt@gmail.com>
2016-12-17 02:27:05 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -33,10 +33,10 @@ function loadCommentForm () {
         };
         
         // Make the request
-        var comment = document.getElementById('comment_text').value;
+        var e_comment = document.getElementById('comment_text').value;
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));  
+        request.send(JSON.stringify({comment: e_comment}));  
         submit.value = 'Submitting...';
         
     };
