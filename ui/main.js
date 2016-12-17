005e4711da29ff94f8d9175ae8e2fa17ebfa84af
Sana Jahan <sanadgrt@gmail.com>
2016-12-18 00:29:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -214,10 +214,10 @@ function loadArticles () {
                     }
                     body{
                          background: red; /* For browsers that do not support gradients */
-                         background: -webkit-linear-gradient(#FFB6C1,#FFA07A); /* For Safari 5.1 to 6.0 */
+                         background: -webkit-linear-gradient(#FAF2FD,#E4BFF4); /* For Safari 5.1 to 6.0 */
                          background: -o-linear-gradient(#FAF2FD,#E4BFF4); /* For Opera 11.1 to 12.0 */
-                         background: -moz-linear-gradient(#FFB6C1,#FFA07A); /* For Firefox 3.6 to 15 */
-                         background: linear-gradient(#FFB6C1,#FFA07A); /* Standard syntax */
+                         background: -moz-linear-gradient(#FAF2FD,#E4BFF4); /* For Firefox 3.6 to 15 */
+                         background: linear-gradient(#FAF2FD,#E4BFF4); /* Standard syntax */
                     }
                      </style>
                       </head>
