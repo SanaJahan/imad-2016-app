70434d55ab2e6dcb17e2d799796d5fbbb925d97a
Sana Jahan <sanadgrt@gmail.com>
2016-12-19 00:11:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -357,7 +357,7 @@ app.get('/check-login',function(req,res){
        res.send(req.session.auth.username.toString());
    }
    else{
-       res.send('You are not looged in');
+       res.send('You are not logged in');
    }
 });
 
@@ -410,7 +410,7 @@ app.get('/get-comments/:articleName',function(req,res){
 
 app.post('/submit-comment/:articleName', function (req, res) {
    // Check if the user is logged in
-    if (req.session && req.session.auth && req.session.auth.userId) {
+    if (req.session && req.session.auth && req.session.auth.userId && req.session.auth.username) {
         // First check if the article exists and get the article-id
         pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
             if (err) {
