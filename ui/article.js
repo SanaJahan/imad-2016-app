6bb4eedfd51ec9e1424036409d7278ce41a874c4
Sana Jahan <sanadgrt@gmail.com>
2016-12-19 21:44:26 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -3,17 +3,7 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
     var commentFormHtml = `
-         <style>#comment_text {
-              padding: 10px;
-              border: solid 1px #dcdcdc;
-              transition: box-shadow 0.3s, border 0.3s;
-            }
-             #comment_text :focus,
-             {
-              border: solid 1px #707070;
-              box-shadow: 0 0 5px 1px #969696;
-            }</style>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <textarea id="comment_text" rows="6" cols="60" placeholder="Enter your comment here..."></textarea>
         <br/><br/>
        <style>#submit{
               display: inline-block;
