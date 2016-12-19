d5aab5d6d85243c2cb936d38cd7c1456e1ab0695
Sana Jahan <sanadgrt@gmail.com>
2016-12-19 22:16:56 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -57,6 +57,7 @@ function loadCommentForm () {
         
         // Make the request
         var comment = document.getElementById('comment_text').value;
+        console.log("Could not read file: " + err)
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({comment:comment}));  
