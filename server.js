5869ea9f1c76f2be36abdd45c17942a7bcf9d43a
Sana Jahan <sanadgrt@gmail.com>
2016-12-19 22:18:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -427,6 +427,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
                         function (err, result) {
                             if (err) {
                                 res.status(500).send(err.toString());
+                                console.log("Could not read file: " + err)
                             } else {
                                 res.status(200).send('Comment inserted!')
                             }
